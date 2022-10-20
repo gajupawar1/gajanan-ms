e4abc5c0b8db237d5c9c8f80b5e56b0fe01d7fc6
unknown <ksv1396@gmail.com>
2022-10-21 02:08:57 +0530

    codeRefactor

---
+++ src/main/java/com/example/gajanan/GajananApplication.java
@@ -0,0 +1,13 @@
+package com.example.gajanan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GajananApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GajananApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gajanan/GajananApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gajanan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GajananApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
